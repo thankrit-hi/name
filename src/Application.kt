d4d0c71d8baf883cd66baa7b491dfb5f1d14ba2b
krit <kinonta@gmail.com>
2020-12-02 22:48:51 +0700

    krihahahha

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,39 @@
 package com.name
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    install(DefaultHeaders)
+    install(CallLogging)
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    install(Routing) {
+        get(path = "/api/krit/name") {
+            val name = Name(
+                name = "see",
+                age = 22,)
+                call.respond(name)
+
+        }
+    }
 }
 
+
+data class Name(
+    val name: String? = null,
+    val age: Int? = null,
+)
